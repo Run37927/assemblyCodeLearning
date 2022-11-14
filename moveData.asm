fc5f55bfccff9b951ffa8cf6b7cc999cc5dce450
Hairun Huang <hairunhuang@gmail.com>
2022-11-14 08:36:53 -0800

    Update moveData.asm

--- moveData.asm
+++ moveData.asm
@@ -1,6 +1,6 @@
 .global _start
 _start:
-    mov r0, #30 ; put 30 in r0
+    mov r0, #30 ; put 30 in r0 (immediate addressing)
     mov r7, #1  ; put 1 into r7
     swi 0       ; software interact
 
