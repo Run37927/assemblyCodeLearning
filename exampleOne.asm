6a549cef773639703883339ea80f6dd79b73bbcd
Hairun Huang <hairunhuang@gmail.com>
2022-11-14 09:01:03 -0800

    subtraction in place

--- exampleOne.asm
+++ exampleOne.asm
@@ -2,4 +2,5 @@ global _start
 _start:
     mov eax, 1      ; move intger 1 into general purpose register named eax
     mov ebx, 42     ; move integer 42 into register ebx
+    sub ebx, 29     ; 42 subtract 29
     int 0x80        ; performs interrup. processor is transferring control to an interrupt handler that we specified here, which is 0x80, which is a system call
