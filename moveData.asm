86ea9f7bab778b6263cd1dca1130299618bcd6a1
Hairun Huang <hairunhuang@gmail.com>
2022-11-14 08:37:52 -0800

    Update moveData.asm

--- moveData.asm
+++ moveData.asm
@@ -2,6 +2,7 @@
 _start:
     mov r0, #30 ; put 30 in r0 (immediate addressing)
     mov r7, #1  ; put 1 into r7
+    move r1, r0 ; moving between two registers, is called register direct addresing
     swi 0       ; software interact
 
 
